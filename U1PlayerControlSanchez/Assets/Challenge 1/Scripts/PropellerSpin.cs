8f2bf32a8cba1b29d741ddd4f1640a2ad085a8cf
Mrpotatoe22 <726318@student.dusd.net>
2025-03-14 09:58:40 -0700

    Finished Unit car challenge
    Doing plane challenge

---
+++ U1PlayerControlSanchez/Assets/Challenge 1/Scripts/PropellerSpin.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PropellerSpin : MonoBehaviour
+{
+    private float propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        propeller = Input.GetAxis("Vertical");
+        transform.Rotate(Vector3.forward * propeller * 40);
+    }
+}
